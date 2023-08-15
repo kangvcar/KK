70223ec396feecb6eef9adf7b782801b5d318022
Kangvcar <kangvcar@126.com>
2023-08-15 12:39:18 +0800

    sortByDate: true

--- blog.config.js
+++ blog.config.js
@@ -10,10 +10,10 @@ const BLOG = {
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
-  path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2015, // If leave this empty, current year will be used.
+  path: '/about', // leave this empty unless you want to deploy Nobelium in a folder
+  since: 2016, // If leave this empty, current year will be used.
   postsPerPage: 6,
-  sortByDate: false,
+  sortByDate: true,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
